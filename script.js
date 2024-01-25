551ffc3f087bfc563db5abe4d78ec78f61d76c94
lexmanthefirst <alex.okhitoya@lifesci.uniben.edu>
2024-01-25 17:58:24 -0500

    experimenting with nav

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
 const nav5 = document.getElementById("nav-5");
+const nav = document.querySelectorAll(".nav_text");
 
 const navItems = [nav1, nav2, nav3, nav4, nav5];
 
@@ -42,9 +43,23 @@ navItems.forEach((nav) => {
   nav.addEventListener("click", toggleNav);
 });
 
-function navColorClicked() {
-  navItems.forEach((nav) => {
-    nav.style.color = "#fff";
+// function navColorClicked() {
+//   navItems.forEach((nav) => {
+//     nav.style.color = "#fff";
+//   });
+// }
+// nav.addEventListener("click", navColorClicked);
+
+const navArr = Array.from(nav);
+
+let selectedNav = null;
+
+navArr.forEach((nav) => {
+  nav.addEventListener("click", function () {
+    if (selectedNav !== null) {
+      selectedNav.style.color = ""; // Set the initial color
+    }
+    nav.style.color = "#fff"; // Set the white color
+    selectedNav = nav; // Update the selected nav item
   });
-}
-nav.addEventListener("click", navColorClicked);
+});
