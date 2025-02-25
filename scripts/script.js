24cc835ec7b5bede6517cd0c9ebd4f18dde301df
Okhitoya Alex <lexmanthefirst@gmail.com>
2025-02-25 14:07:11 -0500

    Added social icons to nav

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,11 @@ const nav1 = document.getElementById("nav-1");
 const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
-// const nav5 = document.getElementById("nav-5");
+const nav5 = document.getElementById("nav-5");
 const nav = document.querySelectorAll(".nav_text");
 
-const navItems = [nav1, nav2, nav3, nav4];
-// const navItems = [nav1, nav2, nav3, nav4, nav5];
+// const navItems = [nav1, nav2, nav3, nav4];
+const navItems = [nav1, nav2, nav3, nav4, nav5];
 // const navItems = [nav2, nav3, nav4, nav5];
 
 //
